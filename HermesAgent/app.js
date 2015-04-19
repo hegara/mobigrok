c3811ed62ef0d5c6b18624464a93ff1b16a0742f
CD.Wang <jewelry_wolf@hotmail.com>
2015-04-18 22:43:00 -0700

    change test to interactive console and let app.js to accept individual service start request

--- HermesAgent/app.js
+++ HermesAgent/app.js
@@ -4,5 +4,28 @@ var Enlister = require('./enlister');
 var server_config = {enlist_url: 'tcp://127.0.0.1:3000',
                     index_url: 'tcp://127.0.0.1:3001'};
 
-Enlister.start_service(server_config);
-Indexer.start_service(server_config);
+var start_enlister = false;
+var start_indexer = false;
+var started = false;
+
+process.argv.forEach(function (v, i, argv) {
+    if (i<2) return;
+    if (v === "enlist") start_enlister = true;
+    else if (v === "index") start_indexer = true; 
+});
+
+if (!start_indexer && !start_enlister) {
+    // nothing started. by default kick off both
+    start_enlister = true;
+    start_indexer = true;
+}
+
+if (start_enlister) { 
+    Enlister.start_service(server_config);
+    started = true;
+}
+
+if (start_indexer) {
+    Indexer.start_service(server_config);
+    started = true; 
+} 
--- HermesAgent/test.js
+++ HermesAgent/test.js
@@ -1,12 +1,37 @@
 // producer.js
 var zmq = require('zmq')
+  , readline = require('readline')
   , sock_enlist = zmq.socket('push');
 
 sock_enlist.bindSync('tcp://127.0.0.1:3000');
 console.log('Enlist queue bound to port 3000');
-
+/*
 setInterval(function(){
     var value = Math.floor(Math.random()*100);
     console.log('sending enlist task of '+value);
     sock_enlist.send(['name'+value,'url','type']);
 }, 500);
+*/
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', function(line) {
+    if (line === "close" || line === "end" || line === "exit") rl.close();
+    else if (line.match("^enlist")) {
+        console.log("enlist requested...");
+        args = line.split(" ");
+        if (args.length >= 3) {
+            var name = args[1].toString();
+            var url = args[2].toString();
+            var type = args[3]?args[3].toString():"git";
+            console.log('sending enlist task of '+name);
+            sock_enlist.send([name, url, type]);
+        } else {
+            console.log("enlist <name> <url> [<type>]");
+        }
+    }
+    console.log("\n");
+    rl.prompt();
+}).on('close',function(){
+    process.exit(0);
+});
