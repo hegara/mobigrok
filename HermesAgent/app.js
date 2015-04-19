aebd8d9f3cc9f4459a4402f06c0623911d47d3d1
CD.Wang <jewelry_wolf@hotmail.com>
2015-04-19 15:00:48 -0700

    mkdir and some other data validations

--- HermesAgent/app.js
+++ HermesAgent/app.js
@@ -1,12 +1,15 @@
 // worker.js
-var Indexer = require('./indexer');
-var Enlister = require('./enlister');
-var server_config = {enlist_url: 'tcp://127.0.0.1:3000',
-                    index_url: 'tcp://127.0.0.1:3001'};
+var Indexer = require('./indexer')
+  , Enlister = require('./enlister')
+  , path = require('path');
+var server_config = {
+                        enlist_url: 'tcp://127.0.0.1:3000',
+                        index_url: 'tcp://127.0.0.1:3001',
+                        root_folder: path.resolve(path.join(__dirname, '..', 'tmp'))
+                    };
 
 var start_enlister = false;
 var start_indexer = false;
-var started = false;
 
 process.argv.forEach(function (v, i, argv) {
     if (i<2) return;
@@ -14,7 +17,7 @@ process.argv.forEach(function (v, i, argv) {
     else if (v === "index") start_indexer = true; 
 });
 
-if (!start_indexer && !start_enlister) {
+if (process.argv.length<3 && !start_indexer && !start_enlister) {
     // nothing started. by default kick off both
     start_enlister = true;
     start_indexer = true;
@@ -29,3 +32,8 @@ if (start_indexer) {
     Indexer.start_service(server_config);
     started = true; 
 } 
+
+if (!start_indexer && !start_enlister) {
+    console.error('Nothing to start. exiting...');
+    process.exit(0);
+}
--- HermesAgent/enlister.js
+++ HermesAgent/enlister.js
@@ -1,4 +1,6 @@
 var zmq = require('zmq')
+  , path = require('path')
+  , mkdirp = require('mkdirp')
   , sock_enlist = zmq.socket('pull')
   , sock_index = zmq.socket('push')
   , spawn = require('child_process').spawn;
@@ -11,18 +13,28 @@ var Enlister = module.exports = function Enlister(name, url, type) {
     this._type = type;
 };
 
+Enlister.RootFolder = __dirname;
 Enlister.AllowedTypes = ["git", "hg", "svn"];
+Enlister.UrlPatterns = ["^(http[s]?|git|ssh)://", "^(http[s]?|ssh)://", "^(http[s]?|svn)://"];
+Enlister.NameRule = "[a-zA-Z0-9]+[a-zA-Z0-9\-\_\+]*";
+Enlister.MaxNameLength = 255;
 
 // use callback for error reporting or return the object
 Enlister.prototype.prepare = function(callback) {
-    // 1. check data legitimacy (url, type, etc.)
+    // 1. check data legitimacy (url, type, name, etc.)
     var enlister = this;
-    console.log("preparing enlister: %j", enlister);
-    if (Enlister.AllowedTypes.indexOf(enlister._type)<0) {
+    console.info("preparing enlister: %j", enlister);
+    var idx_type = Enlister.AllowedTypes.indexOf(enlister._type);
+    if (idx_type<0) {
         callback("Not allowed type: "+enlister._type);
         return;
+    } else if (!enlister._url.match(Enlister.UrlPatterns[idx_type])) {
+        callback("Not supported url: "+enlister._url);
+        return;
+    } else if (enlister._name.length>Enlister.MaxNameLength || !enlister._name.match(Enlister.NameRule)) {
+        callback("Invalid name["+enlister._name.length+"]: "+enlister._name);
+        return;
     }
-    // TODO: add url check
     // 2. check the existence of tools (git, etc.)
     if (this._type === "git") {
         var git_version = spawn("git", ["--version"]);
@@ -30,21 +42,27 @@ Enlister.prototype.prepare = function(callback) {
             var stdout_text = data.toString();
             if (stdout_text.match("^git version [0-9]+\.[0-9]+")) {
                 // TODO: check version number?
-                console.log("passed git version check!");
+                console.info("passed git version check!");
                 // 3. check the appropriate permission (file access, etc.)
+                enlister._path = path.normalize(path.join(Enlister.RootFolder, enlister._name));
                 // 4. make directory
-                enlister._path = "c:\\path\\to\\source\\"+enlister._name;
-                callback(null, enlister);            
+                mkdirp(enlister._path, function (err) {
+                    if (err) callback(err);
+                    else {
+                        // 5. enlist!
+                        callback(null, enlister);   
+                    }
+                });         
             } else {
-                console.log("Dropped the ball!! Cannot find proper version of git.");
-                console.log("  got: "+stdout_text);
+                console.warn("Dropped the ball!! Cannot find proper version of git.");
+                console.warn("  got: "+stdout_text);
                 callback("Cannot find proper version of git. Found: "+stdout_text);
                 return;
             }
         });
     // TODO: add hg/svn support here.
     } else {
-        console.log("Unknown type of source");
+        console.warn("Unknown type of source");
         callback("Unknown type of source repository: "+enlister._type);
         return;
     }
@@ -53,13 +71,13 @@ Enlister.prototype.prepare = function(callback) {
 // enlist the source code with given type of repo tool
 // and return the folder containing the source via callback
 Enlister.prototype.enlist = function(callback) {
-    console.log("Enlisting "+this._name+"["+this._type+"]:"+this._url);
+    console.info("Enlisting "+this._name+"["+this._type+"]:"+this._url);
     callback(null, this);
 };
 
 Enlister.create = function(data, callback) {
     var enlister = new Enlister(data.name, data.url, data.type);
-    console.log('just created: %j', enlister);
+    console.info('just created: %j', enlister);
     enlister.prepare(function(err, result){
         if (err) callback(err);
         callback(null, result);
@@ -69,20 +87,25 @@ Enlister.create = function(data, callback) {
 Enlister.start_service = function(data, callback) {
     sock_enlist.connect(data.enlist_url);
     sock_index.bindSync(data.index_url);
-    console.log('Worker connected to enlist queue: '+data.enlist_url);
+    console.info('Worker connected to enlist queue: '+data.enlist_url);
+    Enlister.RootFolder = data.root_folder||Enlister.RootFolder;
+    console.info('Enlister is using '+Enlister.RootFolder+' as root.');
     sock_enlist.on('message', function(name, url, type){
+        console.time('enlist-'+name);
         Enlister.create({
             name:name.toString(),
             url:url.toString(),
             type:type.toString()
         }, function(err, result){
-            if (err) console.log('enlist prepare err');
+            if (err) console.error('enlist prepare err:\n'+err);
             else {
                 result.enlist(function(err, result){
-                    if (err) console.log('enlist err');
+                    if (err) console.error('enlist err:\n'+err);
                     else {
-                        console.log('enlist worker: %s[%s]: %s', result._name, result._type, result._url);
-                        sock_index.send([result._path, result._path+"\\..\\"+result._name+"-data\\"]);
+                        console.timeEnd('enlist-'+result._name);
+                        console.info('enlist worker: %s[%s]: %s', result._name, result._type, result._url);
+                        sock_index.send([result._path, 
+                            path.resolve(path.join(result._path,"..",result._name+"=data"))]);
                     }
                 });
             }
