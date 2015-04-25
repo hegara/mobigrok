d76525aca737203863397496a53476fe64183e9a
JewelryWolf <jewelry_wolf@hotmail.com>
2015-04-24 23:41:42 -0700

    fix logic of processing tomcat manager api response

--- HermesAgent/deployer.js
+++ HermesAgent/deployer.js
@@ -11,44 +11,48 @@ var Deployer = module.exports = function Deployer(name, source_code_path, index_
     this._index = index_data_path;
 };
 
+// http://tomcat.apache.org/tomcat-7.0-doc/manager-howto.html#Deploy_a_Directory_or_WAR_by_URL
+Deployer.TomcatAuth = null;
+Deployer.TomcatHostname = 'localhost';
+Deployer.TomcatPort = 8080;
+Deployer.TomcatPath = '/manager/text/deploy?';
+Deployer.TomcatMethod = 'PUT';
+
 // deploy the opengrok folder onto tomcat
 Deployer.prototype.deploy = function(callback) {
-	var deployer = this;
+    var deployer = this;
     var query = {
-      path: deployer._source,
-      war: deployer._index,
-      update: false
+        path: deployer._source,
+        war: 'file:'+deployer._index,
+        // set to true and existing app with same path will be undeployed first
+        update: false, 
     };
     var options = {
-      auth: Deployer.TomcatAuth,
-      hostname: Deployer.TomcatHostname,
-      port: Deployer.TomcatPort,
-      path: Deployer.TomcatPath+querystring.stringify(query),
-      method: Deployer.TomcatMethod,
+        auth: Deployer.TomcatAuth,
+        hostname: Deployer.TomcatHostname,
+        port: Deployer.TomcatPort,
+        path: Deployer.TomcatPath+querystring.stringify(query),
+        method: Deployer.TomcatMethod,
     };
     
     var content = '';
 
     var req = require('http').request(options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-        content += chunk;
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            content += chunk;
 
-      });
-      res.on('err', function (chunk) {
-        grunt.log.error(chunk);
-        done(false);
-      });
-      res.on('end', function (chunk) {
-        if(/^OK.*$/m.test(content)) {
-          grunt.log.writeln(content);
-          done();
-        }
-        else {
-          grunt.log.error(content);
-          done(false);
-        }
-      });
+        });
+        res.on('err', function (chunk) {
+            callback(chunk);
+        });
+        res.on('end', function (chunk) {
+            if (/^OK.*$/m.test(content)) {
+                console.info("deploy done: "+content);
+            } else {
+                callback(content);
+            }
+        });
     });
 
 }
@@ -65,15 +69,15 @@ Deployer.create = function(data, callback) {
 
 Deployer.start_service = function(config, callback) {
     sock_deploy.connect(config.deploy_url);
-    if (!config.tomcat_auth || !config.tomcat_hostname || !config.tomcat_port) {
+    if (!config.tomcat_auth) {
         callback('Require valid tomcat configuration to be set!');
     } else {
-        Deployer.TomcatAuth = config.tomcat_auth;
-        Deployer.TomcatHostname = config.tomcat_hostname||'localhost';
-        Deployer.TomcatPort = config.tomcat_port||8080;
-        Deployer.TomcatPath = config.tomcat_path||'/manager/text/deploy?';
-        Deployer.TomcatMethod = config.tomcat_method||'PUT';
         console.info('Worker connected to deploy queue: '+config.deploy_url);
+        Deployer.TomcatAuth = config.tomcat_auth;
+        Deployer.TomcatHostname = config.tomcat_hostname||Deployer.TomcatHostname;
+        Deployer.TomcatPort = config.tomcat_port||Deployer.TomcatPort;
+        Deployer.TomcatPath = config.tomcat_path||Deployer.TomcatPath;
+        Deployer.TomcatMethod = config.tomcat_method||Deployer.TomcatMethod;
         sock_deploy.on('message', function(name, sourcecode, index_data){
             console.time('deploy-'+name);
             Deployer.create({
@@ -92,4 +96,4 @@ Deployer.start_service = function(config, callback) {
             });
         });
     }
-};
+}
