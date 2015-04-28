70b657ced28db35cd93d98c3de2ff4b66f979e89
JewelryWolf <jewelry_wolf@hotmail.com>
2015-04-28 07:41:02 -0700

    use config.json to overwrite default server config

--- HermesAgent/app.js
+++ HermesAgent/app.js
@@ -2,19 +2,22 @@
 var Indexer = require('./indexer')
   , Enlister = require('./enlister')
   , Deployer = require('./deployer')
-  , path = require('path');
-var server_config = {
-                        enlist_url: 'tcp://127.0.0.1:3000',
-                        index_url: 'tcp://127.0.0.1:3001',
-                        deploy_url: 'tcp://127.0.0.1:3002',
-                        root_folder: path.resolve(path.join(__dirname, '..', 'tmp')),
-                        opengrok_path: 'C:\\Users\\Chundong\\workspace\\OpenGrok\\dist\\opengrok.jar',
-                        opengrok_war: 'C:\\Users\\Chundong\\workspace\\OpenGrok\\dist\\source.war',
-                        ctags_path: 'C:\\tools\\ctags58\\ctags.exe',                        
-                        tomcat_auth: 'jewelry:hegara',
-                        tomcat_hostname: 'localhost',
-                        tomcat_port: 8080,
-                    };
+  , path = require('path')
+  , fs = require('fs')
+  , _ = require('underscore');
+var default_server_config = 
+{
+    enlist_url: 'tcp://127.0.0.1:3000',
+    index_url: 'tcp://127.0.0.1:3001',
+    deploy_url: 'tcp://127.0.0.1:3002',
+    root_folder: path.resolve(path.join(__dirname, '..', 'tmp')),
+    opengrok_path: 'C:\\Users\\Chundong\\workspace\\OpenGrok\\dist\\opengrok.jar',
+    opengrok_war: 'C:\\Users\\Chundong\\workspace\\OpenGrok\\dist\\source.war',
+    ctags_path: 'C:\\tools\\ctags58\\ctags.exe',                        
+    tomcat_auth: 'jewelry:hegara',
+    tomcat_hostname: 'localhost',
+    tomcat_port: 8080,
+};
 
 var start_enlister = false;
 var start_indexer = false;
@@ -35,28 +38,53 @@ if (process.argv.length<3) {
     start_deployer = true;
 }
 
-if (start_enlister) { 
-    Enlister.start_service(server_config, function(err){
-        console.error('enlist service error:\n'+err);
-    });
-    started = true;
-}
+config_file = path.resolve(__dirname,'config.json');
+fs.exists(config_file, function(exists){
+    if (exists) {
+        fs.readFile(config_file, function(err, data){
+            if (err) {
+                console.warn('Fail to read from config file: '
+                    +config_file+' and fall back to default settings');
+                start_services(default_server_config);
+            } else {
+                var server_config = _.extend(default_server_config, JSON.parse(data));
+                // Resolve the path
+                server_config.root_folder = path.resolve(server_config.root_folder);
+                
+                start_services(server_config);
+            }
+        });
+    } else {
+        console.warn('Fail to find config file: '
+                +config_file+' and fall back to default settings');
+        start_services(default_server_config);
+    }
+});
 
-if (start_indexer) {
-    Indexer.start_service(server_config, function(err){
-        console.error('index service error:\n'+err);
-    });
-    started = true; 
-} 
+function start_services(server_config) {
+    if (start_enlister) { 
+        Enlister.start_service(server_config, function(err){
+            console.error('enlist service error:\n'+err);
+        });
+        started = true;
+    }
 
-if (start_deployer) {
-    Deployer.start_service(server_config, function(err){
-        console.error('deploy service error:\n'+err);
-    });
-    started = true;
-}
+    if (start_indexer) {
+        Indexer.start_service(server_config, function(err){
+            console.error('index service error:\n'+err);
+        });
+        started = true; 
+    } 
 
-if (!started) {
-    console.error('Nothing started. exiting...');
-    process.exit(0);
+    if (start_deployer) {
+        Deployer.start_service(server_config, function(err){
+            console.error('deploy service error:\n'+err);
+        });
+        started = true;
+    }
+
+    if (!started) {
+        console.error('Nothing started. exiting...');
+        process.exit(0);
+    }
 }
